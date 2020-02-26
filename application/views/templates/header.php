879333105a30608cad79694d2bb02db116f2bffd
Mutlu Dönmez <mutlu.donmez@hotmail.com>
2020-02-27 00:02:11 +0300

    minor details

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,13 +1,13 @@
 <html>
     <head>
-        <title>Simple Blog</title>
+        <title>MDBlog</title>
         <link rel="stylesheet" href="https://bootswatch.com/4/slate/bootstrap.min.css">
         <link rel="stylesheet" href="<?php echo base_url();?>assets/css/style.css">
         <script src="https://cdn.ckeditor.com/4.13.1/standard/ckeditor.js"></script>
     </head>
     <body>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="<?php echo base_url();?>">Simple Blog</a>
+            <a class="navbar-brand" href="<?php echo base_url();?>">MDBlog</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
